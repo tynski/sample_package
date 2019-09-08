f7d66022d70481b72ec963a9491923da69d06601
tynski <b.tynski@gmail.com>
2019-09-08 16:45:58 +0200

    sample test added

--- sample_test.py
+++ sample_test.py
@@ -1,8 +1,10 @@
 import unittest
+from sample_package.sub_package1 import my_sum
 
 
-class TestMySum(unittest.TestCase):
+class TestSamplePackage(unittest.TestCase):
+    def test_my_sum(self):
+        self.assertEqual(my_sum([7,9,1]),17)
 
-
-if __name__ = '__main__':
+if __name__ == '__main__':
     unittest.main()
